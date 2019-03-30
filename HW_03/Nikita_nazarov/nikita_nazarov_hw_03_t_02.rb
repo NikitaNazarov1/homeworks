c76264bb3306bdc01de854cdb55f68d54b9f544a
NikitaNazarov1 <nikenor11@gmail.com>
2019-03-31 00:36:59 +0300

    name var and def more understandable (2)

--- HW_03/Nikita_nazarov/nikita_nazarov_hw_03_t_02.rb
+++ HW_03/Nikita_nazarov/nikita_nazarov_hw_03_t_02.rb
@@ -1,24 +1,24 @@
 def task_2(str)
   res = []
   str.each_line do |s|
-    date = s[/[\[].*[\]]/]
-    sender = s[/^.* - -/]
+    d = s[/[\[].*[\]]/]
+    ip = s[/^.* - -/]
     way = s[/T .* H/]
-    if date && sender && way
-      res << modifydate(date) + ' FROM: ' + modifysender(sender) + ' TO:' + modifyway(way)
+    if d && ip && way
+      res << changedate(d) + ' FROM: ' + changeip(ip) + ' TO:' + changeway(way)
     end
   end
   res
 end
 
-def modifydate(dat)
+def changedate(dat)
   dat.delete_prefix!('[').delete_suffix!(']')
 end
 
-def modifyway(des)
+def changeway(des)
   des.delete_prefix!('T').delete_suffix!(' H').upcase
 end
 
-def modifysender(sen)
+def changeip(sen)
   sen.delete_suffix!(' - -')
 end
