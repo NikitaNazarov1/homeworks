67664bacc2d962fce0e2cb0af954abbe1a9c82eb
NikitaNazarov1 <nikenor11@gmail.com>
2019-03-30 20:29:13 +0300

    complete task 3

---
+++ HW_03/Nikita_nazarov/nikita_nazarov_hw_03_t_03.rb
@@ -0,0 +1,27 @@
+require 'date'
+
+def task_3(str)
+  result = 0
+  count = 0
+  actions = []
+  str.each_line do |x|
+    next unless x.include?('Calling core with action:') == true
+
+    count += 1
+    actions << x[/^.* u/].delete_suffix!('u')
+    result = time(actions) if count == 2
+  end
+  puts result.to_s
+end
+
+def time(arr)
+  firstevent = DateTime.strptime(
+    arr[1].insert(-1, '0'),
+    '%Y-%m-%d %H:%M:%S.%L'
+  ).strftime('%Q').to_i
+  secondevent = DateTime.strptime(
+    arr[0].insert(-1, '0'),
+    '%Y-%m-%d %H:%M:%S.%L'
+  ).strftime('%Q').to_i
+  ((firstevent - secondevent) / 1000.0).round(1)
+end
