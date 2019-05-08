e6cb77b6a7df38cac0f590b3734b2d3dcdd4b709
NikitaNazarov1 <nikenor11@gmail.com>
2019-05-08 17:01:48 +0300

    any fix

--- HW_05/Nikita_nazarov/mentor.rb
+++ HW_05/Nikita_nazarov/mentor.rb
@@ -1,35 +1,48 @@
 require_relative 'homework'
 # class mentor
 class Mentor < Human
-  attr_accessor :notification, :subscribes
+  attr_accessor :notification, :subscribes, :notif_arr
   def initialize(_fullname)
     @notification = []
     @subscribes = []
+    @notif_arr = []
     super
   end
 
   def subscribe_to_student(student)
+  if subscribes.to_s.include?(student.fullname)
+    puts 'already subscribe'
+  else
     subscribes << student
     student.subscriber(self)
   end
+end
 
   def notifications
+    notif_arr = []
     notification.each do |v|
       return 'not subscribe' unless @subscribes.to_s.include?(v.student)
 
-      puts "notice:<#{v.status}> from student: #{v.student} sent: #{v.pull_r}"
+      notif_arr << "notice:<#{v.status}> from student: #{v.student} sent: #{v.pull_r}"
     end
+    puts notif_arr
+    notif_arr
   end
 
   def check_homework(homework: Homework.new)
     if homework.code.eql?('qwe')
-      puts 'Correct homework, congratulations!'
+      puts result = 'Correct homework, congratulations!'
     else
-      puts 'Incorrect homework'
+      puts result = 'Incorrect homework'
     end
+    result
   end
 
   def read_notifications!
     notification.map { |val| val.status = 'read' }
+    notif_arr = []
+    notification.each do |v|
+      notif_arr << "notice:<#{v.status}> from student: #{v.student} sent: #{v.pull_r}"
+    end
   end
 end
